fe95239a50dbe9801012a860e410e8b36d32f129
Rashaad R. Randall <rashaad@clickheredigital.com>
2021-03-17 13:20:56 -0500

    fix: migration timestamps

--- src/database/migrations/20210316211542-create-posts.js
+++ src/database/migrations/20210316211542-create-posts.js
@@ -2,36 +2,41 @@ const tableName = "posts";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction(async (transaction) => {
-    await queryInterface.createTable(tableName, {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+    await queryInterface.createTable(
+      tableName,
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        title: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        body: {
+          type: Sequelize.TEXT,
+          allowNull: false,
+        },
+        slug: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+          allowNull: false,
+        },
+        updated_at: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      title: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      body: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-      },
-      slug: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-    }, { transaction });
+      { transaction }
+    );
   }),
 
   down: async (queryInterface) => queryInterface.sequelize.transaction(async (transaction) => {
--- src/database/migrations/20210317012638-create-users.js
+++ src/database/migrations/20210317012638-create-users.js
@@ -33,6 +33,7 @@ module.exports = {
           updated_at: {
             type: Sequelize.DATE,
             allowNull: false,
+            defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
           },
         },
         { transaction }
